d2581580500c25aab3817bc1bb95312182d60cab
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-12-01 15:34:08 +0530

    Title & X-Axis Y-Axis Label

--- MatplotPy.py
+++ MatplotPy.py
@@ -1,8 +1,11 @@
-from matplotlib import pyplot
-
 xAxis = [1,2,3,4,5]
-yAxis = [1,4,9,16,25]
+yAxis = [1,4,12,16,20]
 
 b = pyplot.plot(xAxis , yAxis)
+
+pyplot.title('2D PLot')
+
+pyplot.xlabel('X',fontdict=None)
+pyplot.ylabel('Y')
 
 pyplot.show()
