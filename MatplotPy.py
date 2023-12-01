a75d9165fcc9d28e4e39ce26fc59d95e5d4541cc
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-12-01 15:36:57 +0530

    Marker MarkerSize MarkerColor LineWidth LineColor LineStyle

--- MatplotPy.py
+++ MatplotPy.py
@@ -1,7 +1,7 @@
 xAxis = [0,1,2,3,4,5,6,7,8,9,10]
 yAxis = [0,1,4,7,9,13,17,20,23,26,30]
 
-b = pyplot.plot(xAxis , yAxis)
+pyplot.plot(xAxis , yAxis , color='#ababab' , linewidth=2 , marker='*' , markersize=10 , markeredgecolor='#000' ,linestyle='-')
 
 pyplot.title('2D PLot',fontdict={'fontname':'Comic Sans MS','fontsize':'14'})
 
