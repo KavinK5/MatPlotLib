281d4477bcd7bfcf032e942fa6598131b725e788
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-12-01 15:34:43 +0530

    X-Axis Y-Axis | fontStyle |  Title

--- MatplotPy.py
+++ MatplotPy.py
@@ -3,9 +3,9 @@ yAxis = [1,4,12,16,20]
 
 b = pyplot.plot(xAxis , yAxis)
 
-pyplot.title('2D PLot')
+pyplot.title('2D PLot',fontdict={'fontname':'Comic Sans MS','fontsize':'14'})
 
-pyplot.xlabel('X',fontdict=None)
-pyplot.ylabel('Y')
+pyplot.xlabel('X',fontdict={'fontname':'Comic Sans MS','fontsize':'12'})
+pyplot.ylabel('Y',fontdict={'fontname':'Comic Sans MS','fontsize':'12'})
 
 pyplot.show()
