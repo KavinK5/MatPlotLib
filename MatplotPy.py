59621593ae42dd264c1803e3711016f31b078ece
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-12-09 15:32:35 +0530

    Multiple Plots in the Single Graph

--- MatplotPy.py
+++ MatplotPy.py
@@ -2,13 +2,25 @@ import matplotlib
 from matplotlib import pyplot
 import numpy
 
-pyplot.resize(figsize = (6 , 4) , dip = 200) #for length and width of the graph
-#Include this immediately after the importing code line
+pyplot.figure(figsize=(6,4),dpi=200)
 
-x1 = numpy.arange(1,11,2)
-y2 = numpy.arange(2,12,2)
+xAxis = numpy.arange(1,20,2)
+yAxis = numpy.arange(21,40,2)
 
-pyplot.plot(x1 , y1 , label = "1x")
+x2Axis = [3,5.0,7.5,10,12.5,15,17.5]
+y2Axis = [22,30,32.5,28,30,38,30]
 
-pyplot.legend() #Code for the legend to be shown
+pyplot.plot(xAxis , yAxis , color='#ababab' , linewidth=2 , marker='*', markersize=8 , markeredgecolor='#000' ,linestyle='-', label = '1st')
+pyplot.plot(x2Axis , y2Axis , color = '#000' , marker = '.' , label = '2nd', linestyle='--')
+
+pyplot.title('2D PLot',fontdict={'fontname':'Comic Sans MS','fontsize':'16'})
+
+fontDICT = {'fontname':'Comic Sans MS',
+            'fontsize':'12'}
+
+pyplot.xlabel('X',fontdict = fontDICT)
+pyplot.ylabel('Y',fontdict = fontDICT)
+
+pyplot.legend()
+
 pyplot.show()
