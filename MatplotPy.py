aae116e4ed4863297751137ca92e8ea556533960
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-12-09 15:29:53 +0530

    Graph legend

--- MatplotPy.py
+++ MatplotPy.py
@@ -8,6 +8,7 @@ pyplot.resize(figsize = (6 , 4) , dip = 200) #for length and width of the graph
 x1 = numpy.arange(1,11,2)
 y2 = numpy.arange(2,12,2)
 
-pyplot.plot(x1 , y1)
+pyplot.plot(x1 , y1 , label = "1x")
 
+pyplot.legend() #Code for the legend to be shown
 pyplot.show()
